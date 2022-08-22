045f1a75ffe5bbf0c7daa5e75a737e9b6b2c69c2
hoanghuy-sentai <hoanghuy.emailbox@gmail.com>
2022-08-22 17:06:07 +0700

    push index file

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>test git1</title>
+</head>
+<body>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores eius tenetur magnam vero eaque quasi architecto, ex optio dicta, veritatis illum rerum voluptate. Aperiam corrupti omnis assumenda itaque sed tempore.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores eius tenetur magnam vero eaque quasi architecto, ex optio dicta, veritatis illum rerum voluptate. Aperiam corrupti omnis assumenda itaque sed tempore.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores eius tenetur magnam vero eaque quasi architecto, ex optio dicta, veritatis illum rerum voluptate. Aperiam corrupti omnis assumenda itaque sed tempore.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores eius tenetur magnam vero eaque quasi architecto, ex optio dicta, veritatis illum rerum voluptate. Aperiam corrupti omnis assumenda itaque sed tempore.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores eius tenetur magnam vero eaque quasi architecto, ex optio dicta, veritatis illum rerum voluptate. Aperiam corrupti omnis assumenda itaque sed tempore.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores eius tenetur magnam vero eaque quasi architecto, ex optio dicta, veritatis illum rerum voluptate. Aperiam corrupti omnis assumenda itaque sed tempore.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores eius tenetur magnam vero eaque quasi architecto, ex optio dicta, veritatis illum rerum voluptate. Aperiam corrupti omnis assumenda itaque sed tempore.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores eius tenetur magnam vero eaque quasi architecto, ex optio dicta, veritatis illum rerum voluptate. Aperiam corrupti omnis assumenda itaque sed tempore.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores eius tenetur magnam vero eaque quasi architecto, ex optio dicta, veritatis illum rerum voluptate. Aperiam corrupti omnis assumenda itaque sed tempore.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores eius tenetur magnam vero eaque quasi architecto, ex optio dicta, veritatis illum rerum voluptate. Aperiam corrupti omnis assumenda itaque sed tempore.</p>
+</body>
+</html>
